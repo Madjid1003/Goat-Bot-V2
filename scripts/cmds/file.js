3c4601c0cb6e280e1d3ba183e96b769eeaa1c2d5
Madjid1003 <168553228+Madjid1003@users.noreply.github.com>
2024-05-22 12:39:36 +0100

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,9 +14,9 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100083695043563"];
+    const permission = ["100088416026395"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("🖕(°ㅂ° ╬)🖕|𝘵𝘶 𝘯'𝘦𝘴 𝘱𝘢𝘴 ཋྀɞHåïtånįɞ✨😷' ", event.threadID, event.messageID);
+      return api.sendMessage("🖕(°ㅂ° ╬)🖕|𝘵𝘶 𝘯'𝘦𝘴 𝘱𝘢𝘴 ཋྀɞMadjidɞ✨😷' ", event.threadID, event.messageID);
     }
     
     const fileName = args[0];
